cccd6a62257a0f020a9f97c17d738411d3f459fe
maks <maxxxymko2003@gmail.com>
2023-10-10 19:45:06 +0300

    init

---
+++ src/main/java/com/standing/controller/GreetingController.java
@@ -0,0 +1,36 @@
+package com.standing.controller;
+
+import com.standing.PythonExecutor;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.micronaut.views.rocker.RockerWritable;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+import static io.micronaut.http.MediaType.TEXT_HTML;
+
+@Controller
+@RequiredArgsConstructor
+public class GreetingController {
+
+    private final PythonExecutor executor;
+
+    @Get
+    @Produces(TEXT_HTML)
+    public HttpResponse<?> get() {
+        return HttpResponse.ok(new RockerWritable(views.helloPage.template()));
+    }
+
+    @Post
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(TEXT_HTML)
+    public  HttpResponse<?> post(@Body Map<String, String> args) {
+        System.out.println(args);
+
+        executor.executeCreatingDirectory();
+
+        return HttpResponse.ok(new RockerWritable(views.helloPage.template()));
+    }
+}
